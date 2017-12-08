be593b5be0c1ba794ebdd36c3dddbaf73a63399d
fayrouz-mikhael <31423114+fayrouz-mikhael@users.noreply.github.com>
2017-12-07 18:00:16 -0600

    update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <algorithm>
+#include <utility> 
 
 using namespace std;
 
@@ -13,6 +15,8 @@ void lookup_bigram(const string&, const string&);
 void process(string&);
 
 int main(int argc, char* argv[]) {
+	
+	
 	if (argc != 2) {
 		cout << "Usage: ./calc_bigrams <FILENAME>\n";
 		return 0;
@@ -38,6 +42,23 @@ int main(int argc, char* argv[]) {
 // 2. increment count of that word in unigram_counts
 // 3. increment count of (prevword, word) in bigram_counts
 void calc_bigrams(const string& fn) {
+
+
+	     	std::ifstream file;
+		    file.open(fn);
+			std::string words;
+	     	std::string prevwords;
+	     	
+	     	
+	    	while(file >> words){
+	     	
+            process(words);
+            
+            unigram_counts[words]++; // increment it.
+			bigram_counts[make_pair(prevwords, words)] ++ ;  
+			prevwords == words;
+}
+file.close();
 }
 
 // Given words w1 and w2,
@@ -48,8 +69,41 @@ void calc_bigrams(const string& fn) {
 // 4. print how many times (w1, w2) appears
 // 5. print count(w1, w2)/count(w1)
 // (see example output)
+
 void lookup_bigram(const string& w1, const string& w2) {
+float count;
+string m1 = w1;
+string m2 = w2 ;
+	
+pair <string, string> ss_pair;
+ss_pair = std::make_pair(m1, m2);
+	
+	
+		process(m1);
+        process(m2);
+		std:: cout << m1 << ", " <<m2  << " : " << std ::endl ;
+
+
+if(bigram_counts.find(make_pair(w1, w2)) == bigram_counts.end())
+std :: cout << "Bigram not found" ;
+
+
+for(map<pair<string, string>, float>:: iterator it= bigram_counts.begin() ;  it != bigram_counts.end() ; ++it){
+	
+	ss_pair = it->first;
+	m1 = ss_pair.first;
+	m2 = ss_pair.second;
+	
+	count = bigram_counts[ss_pair]/unigram_counts[m1]; 
+	
+	cout << w1 << "appears " << unigram_counts[m1]<< " times"  << std ::endl;
+	cout << w1 <<"," << w2 << "  appears " << bigram_counts[ss_pair] << " times"  << std ::endl;
+	cout << count << endl;
 }
+return;
+
+}
+
 
 void process(string& str) {
 	for (int i = 0; i < str.length(); ++i) {
