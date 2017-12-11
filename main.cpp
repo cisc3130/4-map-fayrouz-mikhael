d74119f2e1dc06b0e42f29d030dd5a68de1c940f
fayrouz-mikhael <31423114+fayrouz-mikhael@users.noreply.github.com>
2017-12-11 15:04:10 -0600

    main666

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-#include <utility> 
+#include <utility >
 
 using namespace std;
 
@@ -47,20 +47,19 @@ void calc_bigrams(const string& fn) {
 
 	     	std::ifstream file;
 		    file.open(fn);
-		    
 			std::string words;
 	     	std::string prevwords;
-	     	file >> words;
-	process(words);
-	    	while(file){
-	     	file>>prevwords;
-    process(prevwords);
+	     	
+	     	
+	    	while(file >> words){
+	     	
+            process(words);
+            
             unigram_counts[words]++; // increment it.
 			bigram_counts[make_pair(prevwords, words)]++ ;  
-		    prevwords = words;
+			prevwords = words;
 }
 file.close();
-	
 }
 
 // Given words w1 and w2,
@@ -73,49 +72,35 @@ file.close();
 // (see example output)
 
 void lookup_bigram(const string& w1, const string& w2) {
-	
+float countp;
+
 string m1 = w1;
-string m2 = w2;
+string m2 = w2 ;
 
 process(m1);
 process(m2);
-
 std:: cout << m1 << ", " <<m2  << " : " << std ::endl ;
 
-//map<pair<string, string>, float>::iterator it;
+
 pair<string ,string > ss_pair;
 ss_pair = std::make_pair(m1,m2);
 
-auto it = bigram_counts.find(ss_pair);
-if(it == bigram_counts.end()){
-std :: cout << "Bigram not found"  << std::endl; 
-return;	
-}
-else{
-//bigram_counts[ss_pair] +=1 ;
-
-
-cout << m1 << " " <<  "appears " << " " << unigram_counts[m1]<< " times"  << std ::endl;
-
-
-/*it = bigram_counts.begin();
-while( it!=bigram_counts.end() ){
-	ss_pair = it->first;
-	m1 = ss_pair.first;
-    m2= ss_pair.second;
-	countp = it->second;
-++it;
-}*/
-
+auto it =bigram_counts.find(ss_pair);
+if( it!=bigram_counts.end() ){
+	cout << m1 << " " <<  "appears " << " " << unigram_counts[m1]<< " times"  << std ::endl;
 
 cout << m1 <<"," << m2 << " appears " << bigram_counts[ss_pair]<< " times"  << std ::endl;	
 
-//float count = bigram_counts[ss_pair]/unigram_counts[w1];
 cout << float(bigram_counts[ss_pair]/unigram_counts[m1]) << endl;
 
 }
+
+else{
+std :: cout << "Bigram not found"  << std::endl; 
+return;	
 }
 
+}
 void process(string& str) {
 	for (int i = 0; i < str.length(); ++i) {
 		if(isalpha(str[i])) {
