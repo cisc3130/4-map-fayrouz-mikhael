7fad52670da78a29cdbde055a4a9cc1d9c256dc7
fayrouz-mikhael <31423114+fayrouz-mikhael@users.noreply.github.com>
2017-12-10 22:30:13 -0600

    lastitermain

--- main.cpp
+++ main.cpp
@@ -47,9 +47,9 @@ void calc_bigrams(const string& fn) {
 
 	     	std::ifstream file;
 		    file.open(fn);
-		    
 			std::string words;
 	     	std::string prevwords;
+	     	
 	     	
 	    	while(file >> words){
 	     	
@@ -57,7 +57,7 @@ void calc_bigrams(const string& fn) {
             
             unigram_counts[words]++; // increment it.
 			bigram_counts[make_pair(prevwords, words)]++ ;  
-		    prevwords == words;
+			prevwords == words;
 }
 file.close();
 }
@@ -72,30 +72,40 @@ file.close();
 // (see example output)
 
 void lookup_bigram(const string& w1, const string& w2) {
-	
+float countp;
+
 string m1 = w1;
-string m2 = w2;
+string m2 = w2 ;
 
 process(m1);
 process(m2);
-
 std:: cout << m1 << ", " <<m2  << " : " << std ::endl ;
-	
+
+map<pair<string, string>, float>::iterator it;
 pair<string ,string > ss_pair;
 ss_pair = std::make_pair(m1,m2);
-
+m1 = ss_pair.first;
+m2= ss_pair.second;
 
 if(!(bigram_counts[ss_pair])){
-std :: cout << "Bigram not found"  << std::endl; 
+std :: cout << "Bigram not found"  << std::endl;
 return;	
 }
-
-
+ 
 
 cout << m1 << " " <<  "appears " << " " << unigram_counts[m1]<< " times"  << std ::endl;
 
-cout << m1 <<"," << m2 << " appears " << bigram_counts[ss_pair]<< " times"  << std ::endl;	
-cout << float(bigram_counts[ss_pair]/unigram_counts[m1]) << endl;
+
+it = bigram_counts.begin();
+while( it!=bigram_counts.end() ){
+	ss_pair = it->first;
+	countp = it->second;
+++it;
+}
+
+cout << m1 <<"," << m2 << " appears " << countp<< " times"  << std ::endl;	
+
+cout << countp/unigram_counts[m1] << endl;
 
 }
 
