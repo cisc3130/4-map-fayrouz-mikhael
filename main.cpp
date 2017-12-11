ce9e0af23b9b0c844880fe9e55d99f07816e1654
fayrouz-mikhael <31423114+fayrouz-mikhael@users.noreply.github.com>
2017-12-10 21:51:22 -0600

    update567

--- main.cpp
+++ main.cpp
@@ -86,7 +86,7 @@ pair<string ,string > ss_pair;
 ss_pair = std::make_pair(m1,m2);
 
 
-if(bigram_counts.count(make_pair(w1,w2))<0){
+if(!(bigram_counts[make_pair(w1,w2)])){
 std :: cout << "Bigram not found"  << std::endl; 
 return;	
 }
